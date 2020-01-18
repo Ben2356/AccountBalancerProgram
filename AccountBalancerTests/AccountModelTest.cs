36cf9815921471d46158f234e2518b0ed26058f4
Ben2356 <BenM2356@gmail.com>
2020-01-18 13:23:03 -0600

    wip

---
+++ AccountBalancerTests/AccountModelTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using AccountBalancer.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AccountBalancerTests
+{
+    [TestClass]
+    public class AccountModelTest
+    {
+        private AccountModel accountModel;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            accountModel = new AccountModel();
+        }
+
+        [TestMethod]
+        public void AddDeductionSingleItemTest()
+        {
+            float value = 15.00F;
+
+            accountModel.AddDeduction(value);
+
+            Assert.AreEqual(1, accountModel.Deductions.Count);
+            Assert.AreEqual(value, accountModel.Deductions[0].Item1);
+            Assert.AreEqual(0, accountModel.Deductions[0].Item2);
+        }
+
+        [TestMethod]
+        public void AddDeductionSeveralItemTest()
+        {
+            float value0 = 50.00F;
+            float value1 = 20.00F;
+            float value2 = 35.00F;
+
+            accountModel.AddDeduction(value0);
+            accountModel.AddDeduction(value1);
+            accountModel.AddDeduction(value2);
+
+            Assert.AreEqual(3, accountModel.Deductions.Count);
+            Assert.AreEqual(value0, accountModel.Deductions[0].Item1);
+            Assert.AreEqual(0, accountModel.Deductions[0].Item2);
+            Assert.AreEqual(value1, accountModel.Deductions[1].Item1);
+            Assert.AreEqual(1, accountModel.Deductions[1].Item2);
+            Assert.AreEqual(value2, accountModel.Deductions[2].Item1);
+            Assert.AreEqual(2, accountModel.Deductions[2].Item2);
+        }
+
+        [TestMethod]
+        public void RemoveDeductionSingleItemTest()
+        {
+            accountModel.AddDeduction(20.00F);
+            accountModel.RemoveDeduction(0);
+
+            Assert.AreEqual(0, accountModel.Deductions.Count);
+        }
+
+        [TestMethod]
+        public void RemoveDeductionSeveralItemTest()
+        {
+            float deductionValue = 50.00F;
+            accountModel.AddDeduction(20.00F);
+            accountModel.AddDeduction(deductionValue);
+            accountModel.AddDeduction(30.00F);
+            accountModel.RemoveDeduction(0);
+            accountModel.RemoveDeduction(2);
+
+            Assert.AreEqual(1, accountModel.Deductions.Count);
+            Assert.AreEqual(deductionValue, accountModel.Deductions[0].Item1);
+            Assert.AreEqual(1, accountModel.Deductions[0].Item2);
+        }
+
+        [TestMethod]
+        public void RemoveDeductionBadIdTest()
+        {
+            float deductionValue = 10.00F;
+            accountModel.AddDeduction(deductionValue);
+            accountModel.RemoveDeduction(2);
+
+            Assert.AreEqual(1, accountModel.Deductions.Count);
+            Assert.AreEqual(deductionValue, accountModel.Deductions[0].Item1);
+        }
+
+        [TestMethod]
+        public void RemoveDeductionEmptyCollectionTest()
+        {
+            accountModel.RemoveDeduction(0);
+
+            Assert.AreEqual(0, accountModel.Deductions.Count);
+        }
+
+        [TestMethod]
+        public void TotalDeductionsEmptyCollectionTest()
+        {
+            Assert.AreEqual(0, accountModel.TotalDeductions);
+        }
+
+        [TestMethod]
+        public void TotalDeductionsNonEmptyCollectionTest()
+        {
+
+        } 
+    }
+}
---
+++ AccountBalancerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AccountBalancerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AccountBalancerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27b29cd4-8a03-4566-8b1d-ead6ff473859")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
